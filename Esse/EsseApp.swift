02e11782068717697869abbf796e4d878262d864
Luke Kremer <luketkremer@gmail.com>
2021-01-02 09:44:19 -0800

    Initial Commit

---
+++ Esse/EsseApp.swift
@@ -0,0 +1,17 @@
+//
+//  EsseApp.swift
+//  Esse
+//
+//  Created by Luke Kremer on 1/2/21.
+//
+
+import SwiftUI
+
+@main
+struct EsseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
